10dd649a023ba2ab1a8810f2088586d5816df2ac
shubhamgiri729 <shubhamgiri729@gmail.com>
2026-05-26 22:22:33 +0530

    Fix: Update react-scripts and fix missing semicolon in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Nav from './Nav';
 import Project from './Project';
 // import { BrowserRouter, Routes, Route } from 'react-router-dom';
